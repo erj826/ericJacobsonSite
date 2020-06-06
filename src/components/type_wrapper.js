234b47029fe2d378b47b37c9988f3aa0e9751e55
erj826 <erj826@bu.edu>
2020-06-05 21:00:16 -0400

    first changes

---
+++ src/components/type_wrapper.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+import Typed from "typed.js"
+
+class Typer extends Component {
+  componentDidMount() {
+    const { strings } = this.props
+    const options = {
+      strings: strings,
+      typeSpeed: 40,
+      backSpeed: 40,
+      onComplete: self => { self.cursor.remove() },
+    }
+    this.typed = new Typed(this.el, options)
+  }
+
+  componentWillUnmount() {
+    this.typed.destroy()
+  }
+
+  render() {
+    return (
+      <span
+        style={{ whiteSpace: "pre" }}
+        ref={el => {
+          this.el = el
+        }}
+      />
+    )
+  }
+}
+
+export default Typer
