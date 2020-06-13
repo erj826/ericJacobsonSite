e031f8a80cd63d33f961d21ddef2018889c616d7
erj826 <erj826@bu.edu>
2020-06-13 10:00:39 -0400

    fix static path

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "@emotion/styled"
-import EricJacobsonResume from "../documents/EricJacobsonResume.pdf"
+import EricJacobsonResume from "../../static/documents/EricJacobsonResume.pdf"
 import wayfair from "../../static/images/wayfair.png"
 import redhat from "../../static/images/redhat.png"
 import bose from "../../static/images/bose.png"
